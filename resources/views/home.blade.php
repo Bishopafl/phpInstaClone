92b58e86fbe05000e2043287d76464965f1b7224
Adam Lopez <adamf04@gmail.com>
2020-04-08 02:45:47 -0400

    Created an initial project for an instagram clone using php laravel. application already has authentication for user login set up and am in the process of customizing the front end of the users dashboard

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-med-auto p-5">
+            <img class="rounded-circle" style="height:250px;" src="https://instagram.ftpa1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.ftpa1-1.fna.fbcdn.net&_nc_ohc=8EAnBgzIl20AX_1KCyL&oh=3dda8354b0d549ed6410222a2916331b&oe=5EB65EBE" alt="">
+        </div>
+        <div class="col-med-auto col-sm-auto pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div ><strong>289</strong> posts</div>
+                <div> <strong>43.3k</strong> followers</div>
+                <div> <strong>212</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
