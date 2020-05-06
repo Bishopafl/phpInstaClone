872952fe43e8933ae0ca3abce3070ca8b5304672
Adam Lopez <adamf04@gmail.com>
2020-05-05 20:01:17 -0400

    updated the create and store methods on the post controller.  The controller for posts also ensures that someone is signed in before being able to upload a post.  Next steps:  Will go further into ensuring size contraints and image size proportions are verified and allows for front end error handling if not. Stay tuned...

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
-{
+{   
+    // authenticates user first before user has access to posts
+    // if user is not authenticated, redirects to the login screen
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function create() {
         return view('posts.create');
     }
@@ -13,9 +19,12 @@ class PostsController extends Controller
     public function store() {
         // gets data from image and stores it!
         $data = request()->validate([
+            // 'another' => '', shows a non-required field
             'caption' => 'required',
-            'image' => 'required',
+            'image' => ['required', 'image'],
         ]);
+        // get authenticated user, go into posts and create their post!
+        auth()->user()->posts()->create($data);
 
         dd( request()->all() );
     }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    // overrides protected guarded because we are tracking requirements via the posts controller
+    protected $guarded = [];
+    
     public function user() {
         return $this->belongsTo(User::class);
     }
